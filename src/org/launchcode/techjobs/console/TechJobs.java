c3de17682b58167d9f80c7c611a3e3df47fb0be0
Kathryn Tucker <kathryn.tucker94@gmail.com>
2020-08-22 19:03:32 -0500

    Program will now present a message to the user when there are no jobs within their search parameters.

--- src/org/launchcode/techjobs/console/TechJobs.java
+++ src/org/launchcode/techjobs/console/TechJobs.java
@@ -110,14 +110,20 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
-//For loop iterating through the ArrayList to access each HasMap.
-        for (HashMap item : someJobs) {
-            //Adding a visual separation between each job entry.
-            System.out.println("*****");
-            //iterating through each key in the HashMap, print the key item pair.
-            for (Object key : item.keySet()) {
-                System.out.println(key + ":" + item.get(key));
+        //Adding a descriptive message if no jobs are found in a search.
+        if (someJobs.isEmpty()) {
+            System.out.println("No jobs available with those search parameters.");
+        } else {
+            //For loop iterating through the ArrayList to access each HasMap.
+            for (HashMap item : someJobs) {
+                //Adding a visual separation between each job entry.
+                System.out.println("*****");
+                //iterating through each key in the HashMap, print the key item pair.
+                for (Object key : item.keySet()) {
+                    System.out.println(key + ":" + item.get(key));
+                }
             }
         }
     }
-}
+    }
+
